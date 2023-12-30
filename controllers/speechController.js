a78980e2cfdb58dd533880d8c0bf01c05de1527c
Stephen Kelly <stephen.c.kelly@gmail.com>
2023-12-30 15:32:19 -0500

    complete requests on backend

--- controllers/speechController.js
+++ controllers/speechController.js
@@ -1,24 +1,32 @@
 import { Speech } from '../models/speeches.js'
 
-const getSpeech = async () => {
+const getSpeech = async (req, res) => {
+  console.log(`start of get speech`)
   try {
-
+    const speeches = await Speech.find({}, 'title date speakerFirstName speakerLastName')
+    res.json(speeches);
   } catch (error) {
-    throw (error);
+    console.log('there was an error:', error)
+    res.status(500).send(`an error occured in finding the title and speaker name from the database.`)
   }
 }
 
-const getSpeechById = async () => {
+const getSpeechById = async (req, res) => {
   try {
-
+    const id = req.params.speechId
+    const speech = await Speech.findById(id)
+    res.status(201).json(speech);
+    if (!speech) {
+      return res.status(404).send('Speech not found')
+    }
   } catch (error) {
-    throw (error);
+    console.log('there was an error:', error)
+    res.status(500).send(`error, unable to locate speech by Id.`)
   }
 }
-
 
 const createSpeech = async (req, res) => {
-  console.log(`received data;`, req.body)
+  // console.log(`received data;`, req.body)
   try {
     const payload = req.body
     payload.id = await Speech.countDocuments({})+1
@@ -32,19 +40,34 @@ const createSpeech = async (req, res) => {
   }
 }
 
-const updateSpeech = async () => {
+const updateSpeech = async (req, res) => {
   try {
+    let payload = req.body
+    const id = req.params.speechId
+    console.log(payload)
+    const result = await Speech.findById(id)
 
+    for (const key in payload) {
+      if (payload.hasOwnProperty(key) && result[key] !== undefined) {
+        result[key] = payload[key];
+      }
+    }
+    await result.save()
+    res.status(201).json(result);
   } catch (error) {
-    throw (error);
+    console.log('there was an error:', error)
+    res.status(500).send(`an error occured in the updating of this speech in the database.`)
   }
 }
 
-const deleteSpeech = async () => {
+const deleteSpeech = async (req, res) => {
   try {
-
+    const id = req.params.speechId
+    const deleteDoc = await Speech.deleteOne({id})
+    res.status(201).json(`document deleted`);
   } catch (error) {
-    throw (error);
+    console.log('there was an error:', error)
+    res.status(500).send(`an error occured in the updating of this speech in the database.`)
   }
 }
 
--- routes/speech.js
+++ routes/speech.js
@@ -6,16 +6,17 @@ const router = Router()
 // 
 import { getSpeech, getSpeechById, createSpeech, updateSpeech, deleteSpeech } from '../controllers/speechController.js' 
 
-//no payload. Read links to all Speeches
+//no payload. Read links to all Speeches.  
 router.get('/', getSpeech)
 
 //no payload, but takes ID. Read one speech
 router.get ('/:speechId', getSpeechById)
 
+// /api/speech
 router.post ('/', createSpeech)
 
-router.put('/;speechId', updateSpeech)
+router.put('/:speechId', updateSpeech)
 
-router.delete('/;speechId', deleteSpeech)
+router.delete('/:speechId', deleteSpeech)
 
 export default router
