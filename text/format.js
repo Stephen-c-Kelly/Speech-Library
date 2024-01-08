961030efa943de47dd53d8219e838da80cef1ec8
Stephen Kelly <stephen.c.kelly@gmail.com>
2024-01-08 10:14:31 -0500

    removed date formatting

--- text/format.js
+++ text/format.js
@@ -17,11 +17,8 @@ function unpackTextForDisplay(payload) {
     .replace(/\\"/g, '"')    // Unescape double quotes
     .replace(/\\'/g, "'")    // Unescape single quotes
     .replace(/\\\\/g, '\\'); // Unescape backslashes
-  if (payload.date) {
-    const originalDate = new Date(payload.date); 
-    payload.date = originalDate.toISOString().split('T')[0];
-    }  
-   
+  
+  
   return payload
 }
 
@@ -30,19 +27,6 @@ export {
   unpackTextForDisplay
 }
 
-//testing:
-
-// let originalText = `Every tomorrow has two handles; we can take hold of the handle of anxiety, or the handle of faith. And the first battle is won, my brothers and sisters, when we fight for belief in ourselves, and find that it has come to us while we are still battling. We must not allow petty things to color our lives and stimulate them into vast proportions of evil. To dwell on every slight and clutch it close to our breast and nourish it will corrode our thinking. We're on the move now, and as Frederick Dougless said, "Power concedes nothing without a struggle." It never has, and it never will.
-
-// The United States can no longer afford /the luxury of costly morally, \religiously, and ethically wrong racial discrimination. For America needs all of her citizens with their abilities developed to make a fuller contribution to the future. Many problems scream loudly in this country. The thousands of black citizens disenfranchised, living under degrading conditions. The millions of poor in this nation, white and black, who lack the bare rudiments for fruitful living. The rapidly growing numbers of children caught in a web of disillusionment which destroys their will to learn. The increasing numbers of aged who do not even look forward to rest or retirement.
-
-// And...`;
-// let packagedText = packageTextForStorage(originalText);
-// console.log(`packaged text is here:`, packagedText);  // Output: Hello, \"world\"!\\nThis is a test.
-
-// let unpackedText = unpackTextForDisplay(packagedText);
-// console.log(`unpacked:`, unpackedText); // Output: Hello, "world"!
-//                            // This is a test.
 
 
 
